0c3af1367b6b021c5bc54fa0605f53197529e5ed
tcastanie <thibaut.castanie@gmail.com>
2024-06-16 11:28:43 +0200

    🥅 Add custom error page

---
+++ error.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+defineProps<{
+  error: NuxtError
+}>()
+
+const handleError = () => clearError({ redirect: '/' })
+</script>
+
+<template>
+  <div class="flex items-center justify-center h-screen">
+    <UCard :ui="{ base: 'mx-auto text-center' }">
+      <template #header>
+        <UiH1>{{ error.statusCode }}</UiH1>
+      </template>
+      <UiP v-if="error.statusCode === 404">
+        You got lost in space and found a <b>dead link</b> 😱
+        <NuxtImg
+          src="/404-space-link.jpg"
+          alt="dead link in space"
+          width="512"
+          height="512"
+          class="rounded mt-4"
+        />
+      </UiP>
+      <UiP v-else>
+        {{ error.message }}
+      </UiP>
+      <template #footer>
+        <UButton icon="i-ph-flying-saucer" @click="handleError">
+          Go back to earth
+        </UButton>
+      </template>
+    </UCard>
+  </div>
+</template>
