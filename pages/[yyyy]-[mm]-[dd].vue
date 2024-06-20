c22774b8dd04da19e7c24eb956e9661f5d4e755c
tcastanie <thibaut.castanie@gmail.com>
2024-06-20 17:25:31 +0200

    🔍 Remove trailing slash manually SEO module seems to not work with validate in PageMeta 🤔

--- pages/[yyyy]-[mm]-[dd].vue
+++ pages/[yyyy]-[mm]-[dd].vue
@@ -6,6 +6,16 @@ definePageMeta({
     const day = dayjs(`${yyyy}-${mm}-${dd}`, 'YYYY-MM-DD', true)
     return day.isValid() && day.isBefore(dayjs())
   },
+  middleware: [
+    function (to, _from) {
+      if (to.path !== '/' && to.path.endsWith('/')) {
+        const { path, query, hash } = to
+        const nextPath = path.replace(/\/+$/, '') || '/'
+        const nextRoute = { path: nextPath, query, hash }
+        return navigateTo(nextRoute, { redirectCode: 308 })
+      }
+    },
+  ],
   pageTransition: {
     onAfterLeave() {
       useLoadings().stopAllLoadings()
